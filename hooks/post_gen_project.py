09ef5ac93f191ce57340449e3fa765b3e995694f
Piotr Zaniewski <piotrzan@gmail.com>
2024-05-31 13:07:28 +0200

    swap scripts
    Signed-off-by: Piotr Zaniewski <piotrzan@gmail.com>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,41 +1,18 @@
 import os
 import shutil
-from pathlib import Path, PurePath
-from tempfile import TemporaryDirectory
 
-
-def _move_single_file(src_dir: PurePath, dst_dir: PurePath, file_name: str):
-    shutil.move(
-        str(src_dir.joinpath(file_name)),
-        dst_dir.joinpath(file_name),
-        copy_function=lambda x, y: shutil.copytree(
-            x, y, dirs_exist_ok=True, copy_function=shutil.copy2
-        ),
-    )
-
-
-def move_directory_contents(src: PurePath, dst: PurePath):
-    temp_dir = TemporaryDirectory()
-    temp_dir_path = Path(temp_dir.name)
-
-    directory_contents = os.listdir(src)
-    for item in directory_contents:
-        print(f"Moving {item} to {temp_dir_path}")
-        _move_single_file(src, temp_dir_path, item)
-
-    directory_contents.remove(src.name)
-
-    for item in directory_contents:
-        print(f"Moving {item} to {dst}")
-        _move_single_file(temp_dir_path, dst, item)
-
-    os.removedirs(src)
-
-    _move_single_file(temp_dir_path, dst, src.name)
-
+def move_files(src, dst):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, dirs_exist_ok=True)
+        else:
+            shutil.copy2(s, d)
+    shutil.rmtree(src)
 
 if __name__ == "__main__":
-    if "{{ cookiecutter.use_current_directory }}".lower() == "y":
-        src = Path.cwd()
-        assert src.name == "{{ cookiecutter.project_slug }}"
-        move_directory_contents(src, src.parent)
+    # Assuming this script runs in the generated project directory
+    current_directory = os.getcwd()
+    src_directory = os.path.join(current_directory, 'killercoda-assets')
+    move_files(src_directory, current_directory)
